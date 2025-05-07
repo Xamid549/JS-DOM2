32de199c5f615ecb5fbec6ab1be4e5f0cf4c5b43
Xamid549 <xamidramazanob@gmail.com>
2025-05-07 16:01:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let a =document.getElementsByTagName("body")[0];
+ a.style ="background-color: blue";
+
+// const svet=document.createElement("div");
+// svet.style="width:300px; height:700px;background-color: black";
+// a.appendChild(svet)
+
+// const qizil=document.createElement("div");
+// qizil.style="width: 200px; height: 200px; background-color: red; border-radius: 50%; margin-top: -670px; margin-left: 30px";
+// a.appendChild(qizil)
+// const sariq=document.createElement("div");
+// sariq.style="width: 200px; height: 200px; background-color: yellow; border-radius: 50%; margin: 30px; margin-left: 30px";
+// a.appendChild(sariq)
+// const yashil=document.createElement("div");
+// yashil.style="width: 200px; height: 200px; background-color: green; border-radius: 50%; margin-top: -10px; margin-left: 30px";
+// a.appendChild(yashil)
+
+
+const h1=document.createElement("h1");
+
+h1.style="text-align: center; color:yellow; margin-top:400px; font-size:60px";
+h1.textContent=prompt("Ism kiriting");
+a.appendChild(h1)
